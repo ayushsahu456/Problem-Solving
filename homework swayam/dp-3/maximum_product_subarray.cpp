2fc59b2789a0336ae092de9e198d73c43ac61777
ayushsahu456 <113255015+ayushsahu456@users.noreply.github.com>
2024-06-01 14:37:22 +0530

    Add files via upload

---
+++ homework swayam/dp-3/maximum_product_subarray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int mini=1;
+        int maxi=1;
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                swap(maxi,mini);
+            }
+            maxi=max(maxi*nums[i] , nums[i]);
+            mini=min(mini*nums[i] , nums[i]);
+            ans=max(ans,maxi);
+        }
+        return ans;
+    }
+};
